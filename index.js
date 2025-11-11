bdf814d434fd8afd10ddf0a8b9d4d5f698959f62
saifxyzyz <saifmujeeb2603@gmail.com>
2025-11-11 06:36:57 +0530

    making it work

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var modal = document.getElementById("myModal");
-  var link = document.getElementById("openModalLink")
-  var closeBtn = document.getElementById("closebutton")
-  if (link){
-    link.onclick = function(event) {
+  var modal1 = document.getElementById("modal-1");
+  var modal2 = document.getElementById("modal-2")
+  var link1 = document.getElementById("openModalLink1")
+  var link2 = document.getElementById("openModalLink2")
+  var closeBtn1 = document.getElementById("closebutton1")
+  var closeBtn2 = document.getElementById("closebutton2")
+  if (link1){
+    link1.onclick = function(event) {
       event.preventDefault()
-      modal.classList.add('active')
+      modal1.classList.add('active')
     }
   }
-  if (closeBtn) {
-    closeBtn.onclick = function() {
-      modal.classList.remove('active')
+  if (closeBtn1) {
+    closeBtn1.onclick = function() {
+      modal1.classList.remove('active')
     }
   }
-
+  if (link2){
+    link2.onclick = function(event) {
+      event.preventDefault()
+      modal2.classList.add('active')
+  }
+  }
+  if (closeBtn2) {
+    closeBtn2.onclick = function() {
+      modal2.classList.remove('active')
+    }
+  }
 })
